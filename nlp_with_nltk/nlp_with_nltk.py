097a570f4da11b573fbc52350b95a6956dff5c51
B. Bueno <bbueno25@hotmail.com>
2020-03-05 08:40:07 -0700

    Stop Words - Natural Language Processing With Python and NLTK p.2

--- nlp_with_nltk/nlp_with_nltk.py
+++ nlp_with_nltk/nlp_with_nltk.py
@@ -1,1 +1,14 @@
+"""
+DOCSTRING
+"""
+import nltk.corpus as corpus
+import nltk.tokenize as tokenize
 
+example_sentence = 'This is an example highlighting stop word filtration.'
+stop_words = set(corpus.stopwords.words('english'))
+word_list = tokenize.word_tokenize(example_sentence)
+filtered_sentence = []
+for word in word_list:
+    if word not in stop_words:
+        filtered_sentence.append(word)
+print(filtered_sentence)
